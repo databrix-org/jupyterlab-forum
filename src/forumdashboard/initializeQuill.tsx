95110835e4ee04c07d2cddf9803ab4c3b5959cb3
guyq1997 <gyq673516081@gmail.com>
2024-08-02 16:56:09 +0200

    remove header1

--- src/forumdashboard/initializeQuill.tsx
+++ src/forumdashboard/initializeQuill.tsx
@@ -1,13 +1,12 @@
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
-import '../../style/custom-quill.css'
 
 export function initializeQuill(widget: any): Quill {
     const quill = new Quill('#themeDescription', {
       theme: 'snow',
       modules: {
         toolbar: [
-          [{ 'header': [1, 2, false] }],
+          [{ 'header': [2, false] }],
           ['bold', 'italic', 'underline'],
           ['link', 'image'],
           [{ 'list': 'ordered'}, { 'list': 'bullet' }]
